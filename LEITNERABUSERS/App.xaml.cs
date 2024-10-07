87cd38160a18f930a1b12d2ef91acbf7a27583fa
Tenori0n <maeestrod@gmail.com>
2024-10-07 12:37:20 +0500

    develop / init

---
+++ LEITNERABUSERS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LEITNERABUSERS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
